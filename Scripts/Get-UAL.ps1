a3dd3324e430041eb28fa896e5d888ef16e82184
Matthijs Vos <matthijs.vos@northwave.nl>
2025-09-23 15:50:21 +0200

    Lower trace

--- Scripts/Get-UAL.ps1
+++ Scripts/Get-UAL.ps1
@@ -633,7 +633,7 @@ function Get-UAL {
 														{
 															exit 1
 														}
-														Set-PSDebug -Trace 2
+														Set-PSDebug -Trace 1
 														break
 													}
 													if($emptyRetryCount -ge 3) {
